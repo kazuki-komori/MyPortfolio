9dbb0f056d327834660d43f8a98cd45e35c2a5fd
kazuki-komori <komokazu.nankai@gmail.com>
2020-05-22 17:32:55 +0900

    update

---
+++ components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-footer
+    padless
+    absolute
+    class="footer"
+  >
+    <v-card
+      width="100%"
+      flat
+      tile
+      class="lighten-1 white--text text-center"
+      color="#35495eff"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text icons"
+          icon
+        >
+          <v-icon size="32px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-card-text class="mail">
+        <p>kazuki.komori.eng@gmail.com</p>
+      </v-card-text>
+      <v-divider />
+      <v-card-text class="white--text">
+        ©{{ new Date().getFullYear() }} — <strong>Kazuki Komori All Rights Reserved.</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        icons: [
+          'mdi-facebook',
+          'mdi-twitter'
+        ]
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "./assets/mixins";
+  .icons{
+    margin: 0 1%;
+  }
+  .footer{
+    display: block;
+    margin-top: 100px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,56 @@
     class="page"
   >
     <span class="triangle" />
-    <div class="c-container p-first-card">
-      <v-card width="100%" height="500px" light>
-        <p>hogehoge</p>
+    <div class="c-container p-title">
+      <p class="t-title">
+        Welcome to My Portfolio
+      </p>
+    </div>
+    <div class="c-container p-first-card raised">
+      <v-card width="100%" height="100%" light>
+        <div class="c-section_container">
+          <v-card-title class="c-section_title c-v_line text-section-title">
+            <span class="text-section-title-first">A</span>bout Me
+          </v-card-title>
+        </div>
+        <div class="c-section_container">
+          <v-card-text>
+            <v-row>
+              <v-col cols="sm-3">
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg" />
+              </v-col>
+              <v-col class="c-text_container">
+                <p class="text-body p-name">
+                  小森 一輝 / Kazuki Komori
+                </p>
+                <p class="text-body">
+                  大学：同志社大学 文化情報学部 2回
+                </p>
+                <p class="text-body">
+                  専門：統計学・データサイエンス
+                </p>
+                <p class="text-body">
+                  WEBエンジニア歴：フロントエンド・バックエンドエンジニア（実務経験7ヶ月）
+                </p>
+              </v-col>
+            </v-row>
+            <span class="c-text_container">
+              <p class="text-body p-purpose">Web系エンジニア ✕ データサイエンスを用いたビジネスの実践</p>
+            </span>
+          </v-card-text>
+        </div>
       </v-card>
     </div>
-    <div class="c-container">
+    <div class="c-container last-card raised">
       <v-card width="100%" height="500px" light>
-        <p>hogehoge</p>
+        <div class="c-section_container">
+          <v-card-title class="c-section_title c-v_line text-section-title">
+            <span class="text-section-title-first">A</span>bout Me
+          </v-card-title>
+        </div>
+        <div class="c-section_container">
+          <p>hogehoge</p>
+        </div>
       </v-card>
     </div>
   </v-layout>
@@ -21,13 +63,13 @@
 
 <script>
 
-export default {
-  components: {
+  export default {
+    components: {
+    }
   }
-}
 </script>
 <style lang="scss" scoped>
-@import "../assets/mixins";
+  @import "../assets/mixins";
   .page{
     background-color: $clr-back-ground;
   }
@@ -41,7 +83,41 @@ export default {
     top: 0;
     left: 0;
   }
+  .p-title{
+    margin-top: 20%;
+    @include sp{
+      margin-top: 40%;
+    }
+  }
+  .t-title{
+    font-size: 50px;
+    text-align: left;
+    @include sp{
+      text-align: center;
+      font-size: 35px;
+    }
+  }
   .p-first-card{
-    margin-top: 50%;
+    margin-top: 20%;
+  }
+  .last-card{
+    margin-bottom: 20%;
+  }
+  .p-mb{
+    display: block;
+    margin-bottom: 3rem;
+  }
+  .p-name{
+    font-size: 30px;
+    @include sp{
+      font-size: 18px;
+    }
+  }
+  .p-purpose{
+    text-align: center;
+    font-size: 22px;
+    @include sp{
+      font-size: 13px;
+    }
   }
 </style>
