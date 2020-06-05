a65acfce7a75cb99dbea6c4db51f473a2ea5ca02
kazuki-komori <komokazu.nankai@gmail.com>
2020-06-06 00:36:42 +0900

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,27 @@
 <template>
   <v-toolbar color="#60C799" flat class="header">
     <v-toolbar-title class="navigation">
-      Navigation
+      Navigation {{ screenWidth }}
     </v-toolbar-title>
     <v-spacer />
     <v-card flat class="tabs">
-      <v-tabs right>
-        <v-tab v-scroll-to="'#Top'">
+      <v-tabs v-model="tab" right>
+        <v-tab v-scroll-to="'#Top'" href="#Top">
           Top
         </v-tab>
-        <v-tab v-scroll-to="'#About'">
+        <v-tab key="About" v-scroll-to="'#About'" href="#About">
           About
         </v-tab>
-        <v-tab v-scroll-to="'#SkillSet'">
+        <v-tab key="SkillSet" v-scroll-to="'#SkillSet'" href="#SkillSet">
           Skill Set
         </v-tab>
-        <v-tab v-scroll-to="'#Belongs'">
+        <v-tab v-scroll-to="'#Belongs'" href="#Belongs">
           Belongs
         </v-tab>
-        <v-tab v-scroll-to="'#Products'">
+        <v-tab v-scroll-to="'#Products'" href="#Products">
           Products
         </v-tab>
-        <v-tab v-scroll-to="'#Contact'">
+        <v-tab v-scroll-to="'#Contact'" href="#Contact">
           Contact
         </v-tab>
       </v-tabs>
@@ -30,6 +30,66 @@
 </template>
 <script>
 export default {
+  data () {
+    return {
+      tab: '',
+      scrollY: 0,
+      screenWidth: 0,
+      deviceType: 'PC'
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.calculateScrollY)
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
+  },
+  methods: {
+    onScroll (num) {
+      if (this.deviceType === 'PC') {
+        if (num <= 900 && num >= 450) {
+          this.tab = 'About'
+        } else if (num < 450) {
+          this.tab = 'Top'
+        } else if (num > 900 && num <= 4600) {
+          this.tab = 'SkillSet'
+        } else if (num > 4600 && num <= 5700) {
+          this.tab = 'Belongs'
+        } else if (num > 5700 && num <= 6300) {
+          this.tab = 'Products'
+        } else {
+          this.tab = 'Contact'
+        }
+      } else if (this.deviceType === 'tablet') {
+        if (num <= 900 && num >= 250) {
+          this.tab = 'About'
+        } else if (num < 250) {
+          this.tab = 'Top'
+        } else if (num > 900 && num <= 4200) {
+          this.tab = 'SkillSet'
+        } else if (num > 4200 && num <= 5400) {
+          this.tab = 'Belongs'
+        } else if (num > 5400 && num <= 5600) {
+          this.tab = 'Products'
+        } else {
+          this.tab = 'Contact'
+        }
+      }
+    },
+    calculateScrollY () {
+      this.scrollY = window.scrollY
+      this.onScroll(this.scrollY)
+    },
+    handleResize () {
+      this.screenWidth = document.body.clientWidth
+      if (this.screenWidth > 1900) {
+        this.deviceType = 'PC'
+      } else if (this.screenWidth > 1000 || this.screenWidth <= 1900) {
+        this.deviceType = 'tablet'
+      } else {
+        this.deviceType = 'smartPhone'
+      }
+    }
+  }
 }
 </script>
 <style lang="scss">
